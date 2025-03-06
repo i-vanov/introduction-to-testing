a0ef739a0ce28a4677120c128aa73539d817f2c5
Hristo <hristo.ivanov@accso.de>
2025-03-06 19:09:53 +0100

    Use happy-dom to test a button element

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,21 @@
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  let button;
+  beforeEach(() => {
+    button = createButton();
+  })
+  it('should create a button element', () => {
 
-  it('should have the text "Click Me"', () => {});
+    expect(button).toBeInstanceOf(HTMLButtonElement);
+  });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  it('should have the text "Click Me"', () => {
+    expect(button.innerText).toBe("Click Me");
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    button.click();
+    expect(button.textContent).toBe("Clicked!");
+  });
 });
