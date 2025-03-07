1df7bc0865c1396451458db4bf7bebce76b97a1f
Hristo <hristo.ivanov@accso.de>
2025-03-07 18:02:05 +0100

    Spy on console.log

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,5 +1,13 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
+
+// Ensure that the side effect of a function happened
+const logSpy = vi.spyOn(console, 'log')
 
 test('a super simple test', () => {
-  expect(true).toBe(true);
+  console.log("Hello world");
+
+  expect(logSpy).toHaveBeenCalled(); 
+  expect(logSpy).toHaveBeenCalledWith("Hello world");
+  expect(logSpy).toHaveBeenCalledOnce();
+  expect(logSpy).toHaveBeenCalledTimes(1); 
 });
