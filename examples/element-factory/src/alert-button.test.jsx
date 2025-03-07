f49d16567f5d970a278f31c13ec86008e09fcb67
Hristo <hristo.ivanov@accso.de>
2025-03-07 19:18:09 +0100

    alertSpy exercise

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -1,14 +1,33 @@
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
+import { expect, vi } from "vitest"
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
   it('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+  it.only('should trigger an alert', async () => {
+    const handleSubmit = vi.fn();
+    render(<AlertButton onSubmit={handleSubmit} message='Default Message' />)
+    const input = screen.getByLabelText('Message');
+    const button = screen.getByRole('button', { name: /trigger alert/i });
+
+    const alertSpy = vi.spyOn(window, 'alert');
+
+    await act(async() => {
+      await userEvent.clear(input);
+      await userEvent.type(input, "Hello");
+      await userEvent.click(button);
+    });
+
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(handleSubmit).toHaveBeenCalledWith('Hello')
+    // Click on the button
+    // Look and see if alert() was called with the message
+
+  });
 });
