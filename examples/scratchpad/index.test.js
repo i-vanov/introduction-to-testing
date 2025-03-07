c2963df1a107522a040ce833faac648df4a508b0
Hristo <hristo.ivanov@accso.de>
2025-03-07 18:12:50 +0100

    Mock a function

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -3,11 +3,22 @@ import { test, expect, vi } from 'vitest';
 // Ensure that the side effect of a function happened
 const logSpy = vi.spyOn(console, 'log')
 
-test('a super simple test', () => {
+test('a super simple mock test', () => {
+  const mockFn = vi.fn();
+
+  mockFn('Hello world');
+
+  expect(mockFn).toHaveBeenCalled(); 
+  expect(mockFn).toHaveBeenCalledWith("Hello world");
+  expect(mockFn).toHaveBeenCalledOnce();
+  expect(mockFn).toHaveBeenCalledTimes(1); 
+});
+
+test('a super simple spy test', () => {
   console.log("Hello world");
 
   expect(logSpy).toHaveBeenCalled(); 
   expect(logSpy).toHaveBeenCalledWith("Hello world");
   expect(logSpy).toHaveBeenCalledOnce();
   expect(logSpy).toHaveBeenCalledTimes(1); 
-});
+})
