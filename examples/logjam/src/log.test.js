d1e295b2dcbe6422354f53f01815233ce20941c7
Hristo <hristo.ivanov@accso.de>
2025-03-07 19:31:30 +0100

    Use stub and spy for environment-specific testing

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,43 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('logger', () => {
+    describe('development', () => {
+        beforeEach(() => {
+            // Just for these tests the environment would be DEV
+            vi.stubEnv('MODE', 'development');
+        });
+
+        afterEach(() => {
+            vi.restoreAllMocks();
+        });
+
+        it('should log to the console in DEV mode', () => {
+            const logSpy = vi.spyOn(console, 'log');
+
+            log('Hello World');
+
+            expect(logSpy).toHaveBeenCalled();
+            expect(logSpy).toHaveBeenCalledWith("Hello World");
+        })
+    });
+
+    describe('production', () => {
+        beforeEach(() => {
+            // Just for these tests the environment would be DEV
+            vi.stubEnv('MODE', 'production');
+        });
+
+        afterEach(() => {
+            vi.restoreAllMocks();
+        });
+
+        it('should not call console.log in PROD mode', () => {
+                const logSpy = vi.spyOn(console, 'log');
+    
+                log('Hello World');
+    
+                expect(logSpy).not.toHaveBeenCalled();
+        });
+    })
+});
